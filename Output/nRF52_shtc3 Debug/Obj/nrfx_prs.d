209ab69d0efd32aac6c321fe1d57c9e1b6829a82
manojrjadhao <34579431+manojrjadhao@users.noreply.github.com>
2020-07-16 19:26:23 +0530

    Revert "Revert "Project for SHTC3 sensor created in Segger studio""
    This reverts commit 5f844bbb34ca5cd228df6dc9da96edfe5dff2871.

---
+++ Output/nRF52_shtc3 Debug/Obj/nrfx_prs.d
@@ -0,0 +1,59 @@
+Output/nRF52_shtc3\ Debug/Obj/nrfx_prs.o: \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/drivers/src/prs/nrfx_prs.c \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/nrfx.h \
+ C:/nRF5_SDK_15.3.0_59ac345/integration/nrfx/nrfx_config.h \
+ J:/Manoj/indiamtech/nRF52840_shtc3_mesh/nRF52_shtc3/config/sdk_config.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/drivers/nrfx_common.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52b/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52b/include/stddef.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52b/include/__crossworks.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52b/include/stdbool.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/mdk/nrf.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/mdk/nrf52840.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/toolchain/cmsis/include/core_cm4.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/toolchain/cmsis/include/core_cmInstr.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/toolchain/cmsis/include/cmsis_gcc.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/toolchain/cmsis/include/core_cmFunc.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/toolchain/cmsis/include/core_cmSimd.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/mdk/system_nrf52840.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/mdk/nrf52840_bitfields.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/mdk/compiler_abstraction.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/mdk/nrf_peripherals.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/mdk/nrf52840_peripherals.h \
+ C:/nRF5_SDK_15.3.0_59ac345/integration/nrfx/nrfx_glue.h \
+ C:/nRF5_SDK_15.3.0_59ac345/integration/nrfx/legacy/apply_old_config.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/soc/nrfx_irqs.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/soc/nrfx_irqs_nrf52840.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/nrf_assert.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/app_util.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/mdk/compiler_abstraction.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/nordic_common.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/nordic_common.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/app_util_platform.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/nrf_assert.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/app_error.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52b/include/stdio.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/sdk_errors.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/app_error_weak.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/soc/nrfx_coredep.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/soc/nrfx_atomic.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/sdk_errors.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/sdk_resources.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/drivers/nrfx_errors.h \
+ C:/nRF5_SDK_15.3.0_59ac345/modules/nrfx/drivers/src/prs/nrfx_prs.h \
+ C:/nRF5_SDK_15.3.0_59ac345/integration/nrfx/nrfx_log.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/log/nrf_log.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/sdk_common.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52b/include/string.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/sdk_os.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/app_util.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/util/sdk_macros.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/experimental_section_vars/nrf_section.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/strerror/nrf_strerror.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/log/src/nrf_log_internal.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/log/nrf_log_instance.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/log/nrf_log_types.h \
+ C:/nRF5_SDK_15.3.0_59ac345/components/libraries/log/nrf_log_types.h
